989a74613e069b3af9285b5c976a819a563767e4
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-06-22 00:59:43 +0900

    Add mqtt server using mosca

--- mqtt-client.js
+++ mqtt-client.js
@@ -2,12 +2,15 @@ const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://localhost:1883')
 
 client.on('connect', function () {
-  client.subscribe('presence')
-  client.publish('presence', 'Hello mqtt')
+  client.subscribe('M5Stack/tone')
 })
 
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString())
-  client.end()
 })
+
+let count = 0;
+setInterval(()=>{
+  client.publish('M5Stack/tone', String(count++))
+}, 1000)
