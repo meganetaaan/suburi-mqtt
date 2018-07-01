bb27a1d6bfed5ab961253b2ff79df564fa4422fc
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-07-01 21:23:35 +0900

    Add display

--- public/main.js
+++ public/main.js
@@ -2,10 +2,12 @@ class VOscillator {
   constructor() {
     this.ctx = new AudioContext()
     this.osc
+    this.started = false
   }
 
   // 音を鳴らす
   sound(freq) {
+    this.started = true
     // Failed to execute 'start' on 'AudioScheduledSourceNode': cannot call start more than once.
     // というエラーがでるので、音を鳴らす度にオシレーターを再生成する
     this.osc = this.ctx.createOscillator()
@@ -18,9 +20,14 @@ class VOscillator {
   // 音を止める
   stop() {
     if (this.osc != null) {
+      this.started = false
       this.osc.stop()
     }
   }
+}
+
+function getFreqStr(f) {
+  return String(f.toFixed(1))
 }
 
 const oscillator = new VOscillator()
@@ -29,6 +36,15 @@ client.subscribe("M5Stack/tone")
 client.on("message", function (topic, payload) {
   const p = JSON.parse(payload)
   console.log(p)
-  oscillator.stop()
-  oscillator.sound(p.frequency)
+  if (this.started) {
+    oscillator.osc.frequency = p.frequency
+  } else {
+    oscillator.stop()
+    oscillator.sound(p.frequency)
+    const fv = document.querySelector('.frequency-value')
+    if (fv != null) {
+      fv.innerText = getFreqStr(p.frequency)
+    }
+  }
+
 })
